1cbd1e9d7ef7a03026fbc02907fe30e55c35e313
Sansan Syahrul <san.syahrul.7@gmail.com>
2023-12-11 12:25:04 +0700

    ini adalah client dari server program kalkulator

--- src/client.java
+++ src/client.java
@@ -7,7 +7,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class Client {
+public class client {
     private JFrame frame;
     private JTextField nTextField;
     private JTextField rTextField;
